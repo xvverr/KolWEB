3a4625bfaa8ccda36991e16895caa2e5ad69af7a
xvverr <141499242+xvverr@users.noreply.github.com>
2024-07-18 23:33:20 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+  <title>KolWEB</title>
+  <style>
+    <!-- Dodać jakiś CSS -->
+  </style>
+</head>
+<body>
+  <h1>KolWEB</h1>
+  <a href="statystyki.php">Statystyki firm</a>
+</body>
+</html>
